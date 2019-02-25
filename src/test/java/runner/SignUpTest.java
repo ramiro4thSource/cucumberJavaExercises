66e61573c914520cae4dd5ad2f4b8e350bbba4ff
ramiro.pat@4thsource.com <ramiro.pat@4thsource.com>
2019-02-25 15:40:34 -0600

    changing tag to be runned

--- src/test/java/runner/SignUpTest.java
+++ src/test/java/runner/SignUpTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features/", tags="@Booking",glue= {"steps"},
+@CucumberOptions(features = "src/test/java/features/", tags="@Signup",glue= {"steps"},
 plugin = {"pretty", "html:target/bookingFlight/cucumber", "json:target/bookingFlight/cucumber.json"})
 public class SignUpTest {
 
